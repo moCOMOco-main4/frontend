46dc221a5b2da846d9cd3e9018cb8eb1f7fb2bd7
Sangwoo Shin <ssangwoo96@gmail.com>
2025-04-25 17:57:29 +0900

    feat: 메인 페이지 로고 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import Logo from '@images/';
+import Logo from '@images/Logo.png';
 
 export default function Home() {
   return (
     <div className="flex h-screen items-center justify-center text-4xl">
-      <Image src={} />
+      <Image src={Logo} width={200} height={200} alt="로고" />
     </div>
   );
 }
