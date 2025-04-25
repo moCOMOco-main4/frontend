4e5e859e313bd967a6f0253735e606fedc06afe7
Sangwoo Shin <ssangwoo96@gmail.com>
2025-04-25 17:25:25 +0900

    feat: public 폴더에 images 폴더 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center text-4xl">
-      <span className="text-main-default">지</span>
-      <span className="text-main-base">마</span>
-      <span className="text-main-medium">켓</span>
-      <span className="text-main-light">산</span>스
-    </div>
+    <div className="flex h-screen items-center justify-center text-4xl"></div>
   );
 }
