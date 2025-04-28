df667ebcf5f3d62e6d11a71211d078af49bf1976
Namul21 <k0003706@gmail.com>
2025-04-28 14:05:20 +0900

    design:demoMainPageMarkup

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,11 +1,11 @@
-export default function HomePage() {
+export default function Page() {
   return (
     <main className="p-6">
       <h1 className="text-main-primary text-2xl font-bold">
-        모두를 위한 모각코, 모코모코!
+        모두를 위한 모각코. 모코모코!
       </h1>
       <p className="text-main-soft mt-4">
-        👩‍💻 혼자보다 함께, 모코모코에서 모각코를 즐겨보세요!
+        👩‍💻 햐ㅓㅠㄷㅎㄴㅁㄹㅈㅈ
       </p>
     </main>
   );
