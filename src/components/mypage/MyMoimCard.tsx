3124e431bf68c4f00657d32cbe3acfde75b6bd99
seonysun <csh99240@naver.com>
2025-05-07 07:43:18 +0900

    design: 모임 목록 카드 css 수정

--- src/components/mypage/MyMoimCard.tsx
+++ src/components/mypage/MyMoimCard.tsx
@@ -17,28 +17,24 @@ const MyMoimCard = ({ moim }: MoimProps) => {
   const isWishlist = pathname.startsWith('/mypage/wishlist');
 
   return (
-    <div className="flex w-full items-center justify-between rounded-xl border border-main-base bg-[#FFFCFC] p-3">
-      <div className="flex items-center gap-2">
-        <span className="w-16">
-          {moim.is_closed ? (
-            <Button size="xs" color="red" className="w-full">
-              모집완료
-            </Button>
-          ) : (
-            <Button size="xs" className="w-full">
-              모집중
-            </Button>
-          )}
-        </span>
-        <Image src={Logo} className="size-10 rounded-xl" alt="유저 이미지" />
-        <div>
-          <p className="line-clamp-1 font-semibold text-main-header">
-            {moim.title}
-          </p>
-          <p className="line-clamp-1 text-sm text-gray-500">
-            content api 명세서에 엄슴
-          </p>
-        </div>
+    <div className="flex w-full items-center justify-between gap-2 rounded-xl border border-main-base bg-[#FFFCFC] p-3 hover:brightness-110">
+      <span className="w-16">
+        {moim.is_closed ? (
+          <Button size="xs" color="red" className="w-full cursor-default">
+            모집완료
+          </Button>
+        ) : (
+          <Button size="xs" className="w-full cursor-default">
+            모집중
+          </Button>
+        )}
+      </span>
+      <Image src={Logo} className="size-10 rounded-xl" alt="유저 이미지" />
+      <div className="flex-1 cursor-pointer truncate">
+        <p className="font-semibold text-main-header">{moim.title}</p>
+        <p className="text-sm text-gray-500">
+          sdfsdfsdfsdfsdeㅇㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴddddㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴㄴ
+        </p>
       </div>
       <div className="flex items-center gap-3">
         <span className="flex items-center gap-1 text-gray-500">
