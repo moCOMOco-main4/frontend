8dfdda95d74d9dd2be46173310c3a4ca85531868
seonysun <csh99240@naver.com>
2025-05-07 01:13:59 +0900

    feat: 알림 카드 클릭 시 모달창 닫기 이벤트 추가

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -63,26 +63,7 @@ const Header = () => {
       )}
       {isNotiOpen && (
         <Modal variation="notification" onClose={() => setIsNotiOpen(false)}>
-          <Notifications
-            notiList={[
-              {
-                Notification_id: 12,
-                type: '신청',
-                content: '새로운 모임 신청이 도착했어요!',
-                is_read: false,
-                url: '/posts/21/',
-                created_at: '2025-04-25T22:01:00',
-              },
-              {
-                Notification_id: 13,
-                type: '채팅',
-                content: '지안님이 메시지를 보냈어요.',
-                is_read: true,
-                url: '/chat/a1b2c3/',
-                created_at: '2025-04-25T21:55:00',
-              },
-            ]}
-          />
+          <Notifications onClose={() => setIsNotiOpen(false)} />
         </Modal>
       )}
     </header>
