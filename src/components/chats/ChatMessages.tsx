22715b32b2918e67f2dd4664e8eb85ad6e437ac8
seonysun <csh99240@naver.com>
2025-05-11 09:08:15 +0900

    feat: 채팅 API 함수 작성

--- src/components/chats/ChatMessages.tsx
+++ src/components/chats/ChatMessages.tsx
@@ -2,17 +2,46 @@
 
 import ChatMessage from '@/components/chats/ChatMessage';
 import { ChevronLeft, Send } from 'lucide-react';
-import { messages } from '@/mockup/messages';
 import { useChatStore } from '@/store/useChatStore';
+import { useAuthStore } from '@/store/useAuthStore';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { chatOption } from '@/api/options/chatOption';
+import { useState } from 'react';
+import { chatAPI } from '@/api/functions/chatAPI';
 
 type MsgsProps = {
   room_id: string;
 };
 
 const ChatMessages = ({ room_id }: MsgsProps) => {
-  const currentUserId = 2;
+  const currentUserId = useAuthStore(state => state.user?.id!);
+  const { data: messages } = useQuery(chatOption.chatMessages(room_id));
 
   const { exitRoom } = useChatStore();
+
+  const [inputValue, setInputValue] = useState('');
+
+  const queryClient = useQueryClient();
+  const postMessageMutation = useMutation({
+    mutationFn: (content: string) => chatAPI.postMessages(room_id, { content }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['chat', 'messages', room_id],
+      });
+      setInputValue('');
+    },
+    onError: error => {
+      console.error('전송 실패:', error);
+    },
+  });
+
+  const handleSend = () => {
+    if (inputValue.trim() === '') return;
+    postMessageMutation.mutate(inputValue);
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') handleSend();
+  };
 
   return (
     <div className="flex h-full flex-col p-1">
@@ -23,7 +52,7 @@ const ChatMessages = ({ room_id }: MsgsProps) => {
         <span className="ml-1 font-bold">채팅방명</span>
       </div>
       <div className="flex-1 space-y-3 overflow-y-auto py-2">
-        {messages.map(msg => (
+        {messages?.map(msg => (
           <ChatMessage
             key={msg.ChatMessage_id}
             message={msg}
@@ -36,8 +65,15 @@ const ChatMessages = ({ room_id }: MsgsProps) => {
           type="text"
           className="flex-1 border-none text-sm text-gray-700 outline-none"
           placeholder="메시지를 입력하세요."
+          onChange={e => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
-        <Send stroke="gray" size={20} />
+        <Send
+          stroke="gray"
+          size={20}
+          onClick={handleSend}
+          className="cursor-pointer"
+        />
       </div>
     </div>
   );
