ad3f84480862a0bd81063680077d3d926fa7ded5
Sangwoo Shin <ssangwoo96@gmail.com>
2025-05-12 14:44:23 +0900

    feat: 404 페이지 구현 완료

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,14 +5,14 @@ import logo from '@images/Logo.png';
 
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-10 p-4">
+    <div className="flex h-full flex-col items-center justify-center gap-10 p-4">
       <div>
         <p className="text-center text-4xl font-bold">404 </p>
         <p className="text-center text-2xl">page not found</p>
       </div>
       <Image src={logo} alt="" width={200} height={200} />
 
-      <p className="text-center text-2xl">잘못 된 주소 입니다.</p>
+      <p className="text-center text-2xl">잘못된 주소 입니다.</p>
 
       <Link href="/">
         <Button className="text-xl md:text-2xl" size="xl">
