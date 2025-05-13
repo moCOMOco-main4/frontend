e51b7108f55c0bab8d12c540587bbdd9e1555187
seonysun <csh99240@naver.com>
2025-05-14 02:45:07 +0900

    feat: 관심 목록에서 모임 리스트로 라우팅 처리 추가

--- src/app/join/JoinPage.tsx
+++ src/app/join/JoinPage.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { myMoimOption } from '@/api/options/myMoimOption';
+import Button from '@/components/common/button/Button';
 import MyMoimBox from '@/components/mypage/MyMoimBox';
 import MyMoimCard from '@/components/mypage/MyMoimCard';
 import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 export default function JoinPage() {
+  const router = useRouter();
+
   const { data } = useQuery(myMoimOption.joinedList());
   const joinList = data ?? [];
 
@@ -14,9 +18,10 @@ export default function JoinPage() {
       {joinList.length > 0 ? (
         joinList.map(moim => <MyMoimCard key={moim.id} moim={moim} />)
       ) : (
-        <p className="text-main-soft text-center text-sm">
-          참여중인 모임이 없어요
-        </p>
+        <div className="flex h-full flex-col items-center justify-center gap-4">
+          <p className="text-center">참여중인 모임이 없어요</p>
+          <Button onClick={() => router.push('/moims')}>구경 가기</Button>
+        </div>
       )}
     </MyMoimBox>
   );
