f895f9753b9b631a7ef64efbd8f60161338baab1
seonysun <csh99240@naver.com>
2025-05-16 16:07:45 +0900

    feat: 모달 스토어 타입 옵션 추가

--- src/store/useModalStore.ts
+++ src/store/useModalStore.ts
@@ -1,15 +1,19 @@
 import { create } from 'zustand';
 
+type ModalType = 'confirm' | 'noti' | 'menu' | 'detail' | null;
+
 type ModalState = {
   isOpen: boolean;
-  id: number | null;
-  open: (id: number) => void;
+  type: ModalType;
+  id?: number | null;
+  open: (id?: number | null, type?: ModalType) => void;
   close: () => void;
 };
 
 export const useModalStore = create<ModalState>(set => ({
   isOpen: false,
+  type: 'confirm',
   id: null,
-  open: id => set({ isOpen: true, id }),
-  close: () => set({ isOpen: false, id: null }),
+  open: (id = null, type = 'confirm') => set({ isOpen: true, id, type }),
+  close: () => set({ isOpen: false, id: null, type: null }),
 }));
