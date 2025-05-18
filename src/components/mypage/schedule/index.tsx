f3a6c5ebe57a6c266dc7e9eed47ad5e949509f50
seonysun <csh99240@naver.com>
2025-05-18 15:19:37 +0900

    feat: 스케줄 리스트 API 연결

--- src/components/mypage/schedule/index.tsx
+++ src/components/mypage/schedule/index.tsx
@@ -2,11 +2,11 @@
 
 import { useState } from 'react';
 import 'react-calendar/dist/Calendar.css';
-import '../../../styles/schedule.css';
+import '@/styles/schedule.css';
 import { Schedule } from '@/types/schedule';
 import MyMoimBox from '@/components/mypage/MyMoimBox';
 import Calendar from 'react-calendar';
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { scheduleOption } from '@/api/options/scheduleOptions';
 import { myMoimOption } from '@/api/options/myMoimOption';
 
@@ -19,72 +19,56 @@ const MySchedule = () => {
   const { data: joined } = useQuery(myMoimOption.joinedList());
   const joinedMoim = joined?.map(moim => moim.id) ?? [];
 
-  const { data: schedules } = useQuery(
-    scheduleOption.scheduleList(joinedMoim[0]),
-  );
+  const { data: schedules } = useQuery(scheduleOption.scheduleList(joinedMoim));
 
-  const getSchedulesForDate = (date: Date) => {
-    const dateString = date.toISOString().split('T')[0];
-    return schedules?.filter((schedule: Schedule) =>
-      schedule.date.startsWith(dateString),
-    );
-  };
+  // const getSchedulesForDate = (date: Date) => {
+  //   const dateString = date.toISOString().split('T')[0];
+  //   return schedules?.filter((schedule: Schedule) =>
+  //     schedule.date.startsWith(dateString),
+  //   );
+  // };
+  // const selectedSchedules = !Array.isArray(date)
+  //   ? getSchedulesForDate(date)
+  //   : [];
 
-  const getTileClassName = ({ date }: { date: Date }) => {
-    const dateSchedules = getSchedulesForDate(date);
-    if (dateSchedules?.length === 0) return null;
-    return 'has-schedule';
-  };
+  // const getTileClassName = ({ date }: { date: Date }) => {
+  //   return selectedSchedules?.length > 0 ? 'has-schedule' : '';
+  // };
 
   const handleDateChange = (newDate: Value) => {
     setDate(newDate);
   };
 
-  const queryClient = useQueryClient();
-  const postScheduleMutation = useMutation(
-    scheduleOption.postSchedule(23, queryClient),
-  );
-
   return (
     <MyMoimBox title="일정 관리">
       <div className="rounded-2xl bg-white p-6">
-        <div>
-          <div className="mb-4 flex items-center justify-end gap-4">
-            <div className="flex items-center gap-2">
-              <div className="h-4 w-4 rounded bg-[#E1F0D3]"></div>
-              <span className="text-sm text-gray-600">일정이 있는 날짜</span>
-            </div>
-          </div>
-          <Calendar
-            onChange={handleDateChange}
-            value={date}
-            locale="ko-KR"
-            className="!w-full !border-none"
-            tileClassName={getTileClassName}
-          />
-          <button
-            onClick={() =>
-              postScheduleMutation.mutate({
-                date: '2025-05-18T23:41:58.947Z',
-                memo: '테스트 일정 1',
-              })
-            }
-          >
-            생성
-          </button>
-        </div>
+        <div className="flex items-center justify-end gap-4"></div>
+        <Calendar
+          onChange={handleDateChange}
+          value={date}
+          locale="ko-KR"
+          className="!w-full !border-none font-gmarket"
+          // tileClassName={getTileClassName}
+          navigationLabel={({ date }) => {
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1;
+            return (
+              <span>
+                <span className="font-semibold text-main-dark">{year}</span>년{' '}
+                <span className="font-semibold text-main-dark">{month}</span>월
+              </span>
+            );
+          }}
+        />
         {/* {Array.isArray(date) ? null : (
           <div className="mt-4 space-y-2">
-            {getSchedulesForDate(date)?.length > 0 ? (
-              getSchedulesForDate(date).map(schedule => (
+            {selectedSchedules?.length > 0 ? (
+              selectedSchedules.map(schedule => (
                 <div
                   key={schedule.id}
                   className="rounded-lg border px-4 py-2 shadow-sm"
                 >
-                  <p className="font-semibold text-main-dark">
-                    {schedule.title}
-                  </p>
-                  <p className="text-sm text-gray-500">{schedule.place}</p>
+                  <p className="font-semibold text-main-dark"></p>
                 </div>
               ))
             ) : (
@@ -94,6 +78,16 @@ const MySchedule = () => {
             )}
           </div>
         )} */}
+      </div>
+      <div className="mt-3 flex gap-4">
+        <div className="flex items-center gap-2">
+          <div className="h-4 w-4 rounded border-2 border-[#9bb782]"></div>
+          <span className="text-sm text-gray-600">일정이 있는 날짜</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <div className="h-4 w-4 rounded bg-[#9bb782]"></div>
+          <span className="text-sm text-gray-600">선택한 날짜</span>
+        </div>
       </div>
     </MyMoimBox>
   );
